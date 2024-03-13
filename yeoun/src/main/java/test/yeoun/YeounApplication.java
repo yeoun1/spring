a0bd90a20cfb4ebe4f577ddb025202cf42d3fe3f
yeoun1 <159863304+yeoun1@users.noreply.github.com>
2024-03-13 11:51:59 +0900

    Add files via upload

---
+++ yeoun/src/main/java/test/yeoun/YeounApplication.java
@@ -0,0 +1,13 @@
+package test.yeoun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeounApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeounApplication.class, args);
+    }
+
+}
---
+++ yeoun/src/test/java/test/yeoun/YeounApplicationTests.java
@@ -0,0 +1,13 @@
+package test.yeoun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeounApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
